94357a24d17de762661a26f9374dee8174fb91c2
rakaldi10 <rakaldi10@gmail.com>
2022-07-19 10:29:35 +0700

    Tugas 2 PHP
    Tugas ini saya kumpulkan untuk NIOMIC

---
+++ tugas_2-1.php
@@ -0,0 +1,22 @@
+<?php
+//tipe data casting
+// $angka = 80;
+// $angka2 = 75;
+// $strAngka = (string) $angka;
+// var_dump($strAngka);
+// echo "<br>$strAngka";
+// $mod = null;
+// $mod = $angka % $angka2;
+// echo "<br>Hasil Modulus : $mod";
+
+// mengerjakan 1 unit rumah dibutuhkan waktu 36 hari dengan 12 tenaga kerja.
+//  Berapa waktu akan dihabiskan bila menggunakan 24 orang tenaga kerja?
+
+$angka_1 = 36; //hari 1
+$angka_2 = 12; //tenaga kerja 1
+$angka_3 = 24; //tenaga kerja 2
+$hasil = $angka_3 * $angka_1 / $angka_2;
+echo "$angka_1 hari = $angka_2 Pekerja<br>";
+echo "X = $angka_3 Pekerja<br>";
+echo "X = ...?<br>";
+echo "Jawaban : $hasil";
---
+++ tugas_2-2.php
@@ -0,0 +1,12 @@
+<?php
+// Putri membeli boneka seharga Rp. 50.000. 
+// Kemudian, boneka dijual lagi dengan harga Rp. 80.000. 
+// Berapa persen keuntungan Putri? 
+$jual = 80000;
+$beli = 50000;
+$untung = $jual - $beli;
+$persenUntung = $untung / $beli * 100;
+echo "Jual = $jual<br>";
+echo "Beli = $beli<br>";
+echo "Keuntungan = ?<br>";
+echo "Jawaban : $persenUntung %<br>";
